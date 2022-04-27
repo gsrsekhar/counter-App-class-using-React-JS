27396269e5bdba2c722ac263d25957851f79cb21
gsrsekhar <sekhargondesi@gmail.com>
2022-04-27 05:32:23 +0000

    Counter App Class using React JS

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import Counter from './components/Counter'
+
+const App = () => {
+  return <Counter name="Rajasekhar" />
+}
+export default App
---
+++ src/components/Welcome/index.js
@@ -0,0 +1,11 @@
+import {Component} from 'react'
+
+class Welcome extends Component {
+  render() {
+    const {name} = this.props
+
+    return <h1>{name}</h1>
+  }
+}
+
+export default Welcome
